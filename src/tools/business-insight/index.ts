5d2842ac893327b115209ca8a44dfd2e414c2364
Keerthana B <keerthana.b@intimetec.com>
2025-07-14 21:03:26 +0530

    generate business insight report

--- src/tools/business-insight/index.ts
+++ src/tools/business-insight/index.ts
@@ -1,3 +1,7 @@
 import PeriodicActualVsBudgetTool from "./periodic-actual-vs-budget.tool.js";
+import GenerateBusinessInsightReportTool from "./generate-business-insight-report.tool.js";
 
-export const BusinessInsightTools = [PeriodicActualVsBudgetTool];
+export const BusinessInsightTools = [
+  PeriodicActualVsBudgetTool,
+  GenerateBusinessInsightReportTool,
+];
