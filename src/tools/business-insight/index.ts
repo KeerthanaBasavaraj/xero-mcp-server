d4b09e2d96918e4cbafed3c4dca8f153bbb694bc
Keerthana B <keerthana.b@intimetec.com>
2025-07-17 07:30:25 +0530

    added basic structure

--- src/tools/business-insight/index.ts
+++ src/tools/business-insight/index.ts
@@ -1,7 +1,9 @@
 import PeriodicActualVsBudgetTool from "./periodic-actual-vs-budget.tool.js";
 import GenerateBusinessInsightReportTool from "./generate-business-insight-report.tool.js";
+import GenerateBusinessInsightReportFullTool from "./generate-business-insight-report-full.tool.js";
 
 export const BusinessInsightTools = [
   PeriodicActualVsBudgetTool,
   GenerateBusinessInsightReportTool,
+  GenerateBusinessInsightReportFullTool,
 ];
