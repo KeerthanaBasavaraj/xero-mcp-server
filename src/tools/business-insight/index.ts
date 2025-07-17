ba4da2e4ae958177e5ed7de27bbf69a1c42ca651
Keerthana B <keerthana.b@intimetec.com>
2025-07-17 08:08:29 +0530

    Remove extra

--- src/tools/business-insight/index.ts
+++ src/tools/business-insight/index.ts
@@ -1,9 +1,7 @@
 import PeriodicActualVsBudgetTool from "./periodic-actual-vs-budget.tool.js";
 import GenerateBusinessInsightReportTool from "./generate-business-insight-report.tool.js";
-import GenerateBusinessInsightReportFullTool from "./generate-business-insight-report-full.tool.js";
 
 export const BusinessInsightTools = [
   PeriodicActualVsBudgetTool,
   GenerateBusinessInsightReportTool,
-  GenerateBusinessInsightReportFullTool,
 ];
